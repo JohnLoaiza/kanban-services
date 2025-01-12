0bd9d4c57660171307243bfd3a27f5c096b68cd1
John <jhon91811@gmail.com>
2025-01-12 11:46:29 -0500

    endpoint inicial

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import taskRoutes from './routes/taskRoutes.js';
 import requerimentsRoutes from './routes/requerimentsRoutes2.js';
 import userRoutes from './routes/userRoutes.js';
 import webhook from './routes/webhook.js';
-
+import DbConnect from './bd/dbConnect.js';
+const router = express.Router();
 
 
 
@@ -18,6 +19,12 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Ruta inicial
+router.get('/', async (req, res) => {
+  DbConnect.bdProcess(res, async () => {
+    res.status(200).json("El API de servicios esta corriendo");
+  });
+});
 
 // DbConnect.open()
 app.use('/api/kanbans', kanbanRoutes);
@@ -26,6 +33,7 @@ app.use('/api/tasks', taskRoutes);
 app.use('/api/requeriments', requerimentsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/webhook', webhook);
+app.use('/api', router);
 
 
 
